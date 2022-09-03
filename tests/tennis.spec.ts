321b574fc7d8aca061aa39d6810859e0360800c1
naharoni <naharoni@fireblocks.com>
2022-09-03 23:29:40 +0300

    init

--- tests/tennis.spec.ts
+++ tests/tennis.spec.ts
@@ -1,16 +1,17 @@
 import {expect, test} from '@playwright/test';
-import {sendEmail} from '../utils/email';
 import {constants} from '../constants';
+import {sendEmail} from '../utils/email';
+
+const path = 'tennis123.png';
 
 test('Tennis test', async ({page}) => {
     await page.goto(constants.url);
 
+    await page.screenshot({path, fullPage: true});
     try {
-        await expect(page).toHaveScreenshot('./tennis.png', {fullPage: true});
+        await expect(page).toHaveScreenshot('tennis.png', {fullPage: true});
         console.log('Test passed');
     } catch (error) {
-        const path = 'tennis123.png';
-        await page.screenshot({path, fullPage: true});
         sendEmail({attachments: [{filename: 'current-screenshot.png', path}]});
     }
 
