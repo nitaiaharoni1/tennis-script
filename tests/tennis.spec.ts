ba52f9a90c49a2d96d5eefd49fa79ff1df93d750
naharoni <naharoni@fireblocks.com>
2022-09-03 22:24:03 +0300

    init

--- tests/tennis.spec.ts
+++ tests/tennis.spec.ts
@@ -6,10 +6,9 @@ test('Tennis test', async ({page}) => {
     await page.goto(constants.url);
 
     try {
-        await expect(page).toHaveScreenshot('tennis2.png', {fullPage: true});
+        await expect(page).toHaveScreenshot('tennis.png', {fullPage: true});
         console.log('Test passed');
     } catch (error) {
-        console.log('Sending email');
         const path = 'tennis123.png';
         await page.screenshot({path, fullPage: true});
         sendEmail({attachments: [{filename: 'current-screenshot.png', path}]});
