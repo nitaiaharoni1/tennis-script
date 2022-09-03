9f4dda95ad75446966cd692cd657ae9428c2ad81
naharoni <naharoni@fireblocks.com>
2022-09-04 00:39:44 +0300

    init

--- tests/tennis.spec.ts
+++ tests/tennis.spec.ts
@@ -1,6 +1,7 @@
 import {expect, test} from '@playwright/test';
 import {constants} from '../constants';
 import * as fs from 'fs';
+import {sendEmail} from '../utils/email';
 
 const path = 'tennis123.png';
 
@@ -9,17 +10,17 @@ test('Tennis test', async ({page}) => {
     await page.waitForTimeout(5000);
 
     await page.screenshot({path});
-    const compareScreenshotPath = 'tennis2.png';
+    // const compareScreenshotPath = 'tennis2.png';
     const tennisTxtFileText = fs.readFileSync('tennis.txt', 'utf8');
     const bodyText = await page.innerText('body');
 
     try {
-        // await expect(page).toHaveScreenshot(compareScreenshotPath, {maxDiffPixels: 10});
+        // await expect(page).toHaveScreenshot(compareScreenshotPath, {maxDiffPixels: 0});
         await expect(bodyText).toBe(tennisTxtFileText);
         console.log('Test passed');
     } catch (error) {
         console.log('Found change');
-        // sendEmail({attachments: [{filename: 'current-screenshot.png', path}]});
+        sendEmail({attachments: [{filename: 'current-screenshot.png', path}]});
     }
 
     await page.waitForTimeout(10000);
